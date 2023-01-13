2002614d98167b5e7e72ebcd36a1ba978768579a
mdmusharruf13 <116986038+mdmusharruf13@users.noreply.github.com>
2023-01-13 21:19:28 +0530

    Delete M01_main.java

--- M01_main.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-import java.util.Scanner;
-class M01_main{
-    public static void main(String [] args){
-        System.out.println("Hi, i am musharruf!");
-    }
-}
