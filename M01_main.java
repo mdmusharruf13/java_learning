6eed6f96a76c1a695b10d932f4793b8093c37ac7
Musharruf <mdmusharrufbinjafar13@gmail.com>
2023-01-13 19:50:49 +0530

    first java program

---
+++ M01_main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class M01_main{
+    public static void main(String [] args){
+        System.out.println("Hi, i am musharruf!");
+    }
+}
