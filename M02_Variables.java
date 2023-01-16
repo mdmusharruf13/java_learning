5187c90e437ed4cc8a4cdc65f878741e46e5015c
Musharruf <mdmusharruf13@gmail.com>
2023-01-16 23:20:35 +0530

    variables in java

---
+++ M02_Variables.java
@@ -0,0 +1,12 @@
+class M02_Variables{
+    public static void main(String [] args){
+        int num1 = 598;
+        float num2 = 5.98f;
+        char c = 'm';
+        boolean b = true;
+        String str = "Java";
+        System.out.println(num1+" "+num2);
+        System.out.println(c+" , "+str);
+        System.out.println(b);
+    }
+}
